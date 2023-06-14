844b6280d795bfd82f5d48657766b7a9a02049f1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-14 21:58:45 +0300

    Added UtilHelper folder in the MVCPractice section of a small course on Java EE

---
+++ MVCPractice/src/PartThree/UtilHelper/JspPathHelper.java
@@ -0,0 +1,19 @@
+package PartThree.UtilHelper;
+
+import lombok.experimental.UtilityClass;
+@UtilityClass
+public class JspPathHelper {
+    private static final String JSP_FORMAT = "/WEB-INF/jsp/%s.jsp";
+    private static final String JSTL_FORMAT = "/WEB-INF/JstlDemo/%s.jsp";
+    private static final String JST_FORMS_FORMAT = "/WEB-INF/jspForms/%s.jsp";
+    public static String getJspPath (String jspPageName){
+        return String.format(JSP_FORMAT, jspPageName);
+    }
+    public static String getJstlDempPath (String jstlPageName){
+        return String.format(JSTL_FORMAT, jstlPageName);
+    }
+
+    public static String getJspFormPath (String jstFormPage){
+        return String.format(JST_FORMS_FORMAT, jstFormPage);
+    }
+}
