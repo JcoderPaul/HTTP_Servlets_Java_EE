f266f49a6a3a493507dd34c8db62161987c008f5
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-14 22:36:21 +0300

    Added Exception folder in the MVCPracticeAdvanced section of a small course on Java EE

---
+++ MVCPracticeAdvanced/src/AirportSimulatorTwo/Exception/ValidationException.java
@@ -0,0 +1,15 @@
+package AirportSimulatorTwo.Exception;
+
+import AirportSimulatorTwo.Validator.Error;
+import lombok.Getter;
+
+import java.util.List;
+
+public class ValidationException extends RuntimeException {
+    @Getter
+    private final List<Error> app_errors;
+
+    public ValidationException(List<Error> app_errors){
+        this.app_errors = app_errors;
+    }
+}
