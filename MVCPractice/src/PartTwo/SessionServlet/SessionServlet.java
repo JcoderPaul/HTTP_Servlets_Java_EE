092a51b49b3b07d2be69530bec19462b0a575192
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-14 21:57:31 +0300

    Added SessionServlet folder in the MVCPractice section of a small course on Java EE

---
+++ MVCPractice/src/PartTwo/SessionServlet/SessionServlet.java
@@ -0,0 +1,20 @@
+package PartTwo.SessionServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/try-session")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /* Если cookie хранятся на стороне клиента, то сессия хранится на стороне сервера */
+        HttpSession mySession = req.getSession();
+        System.out.println(mySession.isNew());
+    }
+}
