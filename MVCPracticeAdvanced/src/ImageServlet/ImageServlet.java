7535d3ffc1ba7685ba4e4d226dd6fec91a8b65e3
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-14 22:40:22 +0300

    Added ImageServlet folder in the MVCPracticeAdvanced section of a small course on Java EE

---
+++ MVCPracticeAdvanced/src/ImageServlet/ImageServlet.java
@@ -0,0 +1,47 @@
+package ImageServlet;
+
+import AirportSimulatorTwo.Service.ImageService;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+
+import java.io.InputStream;
+
+@WebServlet("/Image/*")
+public class ImageServlet extends HttpServlet {
+    private final ImageService imageService = ImageService.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        String requestUri = req.getRequestURI();
+        String imagePath = requestUri.replace("/Image","");
+
+        /*
+        Если картинка на сервере есть, то мы возвращаем поток данных в
+        response, перед этим установив тип передаваемого контента и
+        собственно пишем данные в выходной поток методом writeImage,
+        если же ее нет (файла нет) мы возвращаем 404 страницу.
+        */
+        imageService.getImage(imagePath).
+                ifPresentOrElse(image -> {
+                    resp.setContentType("application/octet-stream");
+                    writeImage(image, resp);
+                }, () -> resp.setStatus(404));
+    }
+    /*
+    Метод позволяет передать наш файл (картинку) от сервера к пользователю,
+    т.е. преобразовывает входящий поток байт в исходящий из сервлета поток
+    байт.
+    */
+    @SneakyThrows
+    private void writeImage(InputStream image, HttpServletResponse resp) {
+        try(image; ServletOutputStream outputStream = resp.getOutputStream()){
+            int currentByte;
+            while ((currentByte = image.read()) != -1) {
+                outputStream.write(currentByte);
+            }
+        }
+    }
+}
