c70d4d93f6bd5a0b6302ba424049100a402a95f1
Paul Yermolaeff <100008373+JcoderPaul@users.noreply.github.com>
2024-08-09 17:51:48 +0500

    Update HttpClientExample.java

--- SocketClasses/src/HTTP_Client/HttpClientExample.java
+++ SocketClasses/src/HTTP_Client/HttpClientExample.java
@@ -1,11 +1,10 @@
 package HTTP_Client;
+
 /*
-После Java 11, наиболее предпочтительным классом
-для взаимодействия приложений с HTTP серверами
-является класс HttpClient, а не более ранний класс
-URL. HttpClient потокобезопасен и может работать
-в асинхронном режиме.
+После Java 11, наиболее предпочтительным классом для взаимодействия приложений с HTTP серверами является класс 
+HttpClient, а не более ранний класс URL. HttpClient потокобезопасен и может работать в асинхронном режиме.
 */
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,50 +15,37 @@ import java.util.Map;
 public class HttpClientExample {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        
         /*
-        Создаем HTTP клиент через билдер и задаем версию
-        протокола 1.1. Теперь, используя этот клиент, мы
-        можем отправлять запросы к HTTP серверу.
+        Создаем HTTP клиент через билдер и задаем версию протокола 1.1. Теперь, используя этот клиент, мы можем отправлять запросы к HTTP серверу.
 
-        При этом данный объект уже на стадии создания имеет
-        массу возможностей или методов для обращения к HTTP
-        серверу в различных режимах (аутентификация,
-        соединение по таймауту, выбор приоритета и т.д.).
+        При этом данный объект уже на стадии создания имеет массу возможностей или методов для обращения к HTTP серверу в различных режимах 
+        (аутентификация, соединение по таймауту, выбор приоритета и т.д.).
         */
+        
         HttpClient myFirstHttpClient = HttpClient
                 .newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
-        /*
-        Создаем запрос к HTTP серверу, объект HttpRequest,
-        в котором мы можем определить метод отправки запроса:
-        GET, POST, DELETE и т.д.
-        */
+        
+        /* Создаем запрос к HTTP серверу, объект HttpRequest, в котором мы можем определить метод отправки запроса: GET, POST, DELETE и т.д. */
+        
         HttpRequest myRequest = HttpRequest
                 .newBuilder(URI.create("https://ya.ru"))
                 .GET() // Выбрали метод запроса
                 .build();
-        /*
-        Метод *.send() позволяет отправить запрос к HTTP
-        серверу, при этом есть возможность отправить запрос
-        в синхронном и асинхронном режиме (получив
-        CompletableFuture ответ).
-        */
-        HttpResponse<String> responseToMeFromServer =
-                myFirstHttpClient.send(myRequest, HttpResponse.BodyHandlers.ofString());
+        
+        /* Метод *.send() позволяет отправить запрос к HTTP серверу, при этом есть возможность отправить запрос в синхронном и асинхронном режиме (получив CompletableFuture ответ). */
+        HttpResponse<String> responseToMeFromServer = myFirstHttpClient.send(myRequest, HttpResponse.BodyHandlers.ofString());
+        
         /* Получаем "заголовки" - хедеры отправленные HTTP сервером в ответ на наш запрос */
         Map responseHeaders = responseToMeFromServer.headers().map();
-
         responseHeaders.forEach((key, value) -> {
             System.out.println(key + " : " + value);
         });
-
         System.out.println("\n**************************************************************************\n");
         /* Получаем HTML страницу, сформированную сервером, в ответ на наш запрос */
         String responseBody = responseToMeFromServer.body();
         System.out.println(responseBody);
     }
-
 }
-
-
