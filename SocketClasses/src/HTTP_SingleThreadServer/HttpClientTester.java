439a70f2916c7f0a8ba2885230c8861738833a00
Paul Yermolaeff <100008373+JcoderPaul@users.noreply.github.com>
2024-08-09 18:41:28 +0500

    Update HttpClientTester.java

--- SocketClasses/src/HTTP_SingleThreadServer/HttpClientTester.java
+++ SocketClasses/src/HTTP_SingleThreadServer/HttpClientTester.java
@@ -11,33 +11,24 @@ import static java.net.http.HttpRequest.BodyPublishers.ofFile;
 
 public class HttpClientTester {
     public static void main(String[] args) throws IOException, InterruptedException {
-        /*
-        1. Создаем наш HTTP клиент, который
-           будет формировать запрос к серверу
-        */
+        /* 1. Создаем наш HTTP клиент, который будет формировать запрос к серверу */
         HttpClient myHttpClientToServerTest = HttpClient
                 .newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
-        /*
-        2. Формируем запрос к серверу, в котором мы
-           отправим наш jsom файл из папки 'resources'
-           в качестве запроса. Запрос идет методом
-           POST и содержит тело - наш json файл.
-        */
+        
+        /* 2. Формируем запрос к серверу, в котором мы отправим наш jsom файл из папки 'resources' в качестве запроса. Запрос идет методом POST и содержит тело - наш json файл */
         HttpRequest myRequestToServer = HttpRequest
                 .newBuilder()
                 .uri(URI.create("http://localhost:9999")) // Указываем куда идет запрос и на какой порт
                 .header("content-type", "application/json")
                 .POST(ofFile(Path.of("resources","myFirst.json")))
                 .build();
-        /*
-        3. Отправляем запрос к серверу используя метод *.send(),
-           куда мы передаем наш запрос. Ответом будет обычная
-           html страница - обычный текстовый файл.
-        */
+        
+        /* 3. Отправляем запрос к серверу используя метод *.send(), куда мы передаем наш запрос. Ответом будет обычная html страница - обычный текстовый файл */
         HttpResponse responseFromServer =
                 myHttpClientToServerTest.send(myRequestToServer, HttpResponse.BodyHandlers.ofString());
+        
         /* Выводим ответ сервера на экран */
         System.out.println(responseFromServer.headers());
         System.out.println("\n");
