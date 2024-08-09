1d40d67252910fc3975d43c901fcdad9f76679d5
Paul Yermolaeff <100008373+JcoderPaul@users.noreply.github.com>
2024-08-09 18:47:57 +0500

    Update PoolThreadHttpServer.java

--- SocketClasses/src/HTTP_MultiThreadServer/PoolThreadHttpServer.java
+++ SocketClasses/src/HTTP_MultiThreadServer/PoolThreadHttpServer.java
@@ -1,9 +1,7 @@
 package HTTP_MultiThreadServer;
-/*
-Создаем простой, однопоточный HTTP сервер,
-который принимает некие запросы и генерирует
-некий стандартный ответ.
-*/
+
+/* Создаем простой, многопоточный HTTP сервер, который принимает некие запросы и генерирует некий стандартный ответ */
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -15,27 +13,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class PoolThreadHttpServer {
-    /*
-    Для создания сервера, нам понадобится порт,
-    который он будет прослушивать, этот параметр
-    мы передадим в ServerSocket.
-    */
-    private final int port;
-    /* Создаем пул потоков через ExecutorService */
-    private final ExecutorService socketsPool;
+    private final int port;    // Для создания сервера, нам понадобится порт, который он будет прослушивать, этот параметр мы передадим в ServerSocket
+    private final ExecutorService socketsPool;    // Создаем пул потоков через ExecutorService
     private boolean serverStopped = false;
-    /*
-    Передаем интересующий нас порт через конструктор
-    и количество потоков в которых будут обрабатываться
-    запросы от HTTP клиентов.
-    */
+    
+    /* Передаем интересующий нас порт через конструктор и количество потоков в которых будут обрабатываться запросы от HTTP клиентов */
     public PoolThreadHttpServer(int port, int poolSize) {
         this.port = port;
         this.socketsPool = Executors.newFixedThreadPool(poolSize);
     }
+    
     /* Метод запускающий наш сервер */
     public void runServer(){
-
         try {
             /*
             1. Создаем ServerSocket и передаем ему порт;
@@ -59,25 +48,21 @@ public class PoolThreadHttpServer {
             throw new RuntimeException(exception);
         }
     }
+
     /* Метод обрабатывающий запрос от клиента */
     private void processingSocketRequest(Socket clientSocket){
         /*
-        Открываем два потока входящий и исходящий для
-        получения запросов - request и формирования
-        ответов - response.
+        Открываем два потока входящий и исходящий для получения запросов - request и формирования ответов - response.
 
-        Естественно в реальности сервер ведет более
-        сложную работу по обработке запросов и выдаче
-        соответствующих ответов, в зависимости от
-        полученного запроса. Но суть не меняется.
+        Естественно в реальности сервер ведет более сложную работу по обработке запросов и выдаче соответствующих ответов, 
+        в зависимости от полученного запроса. Но суть не меняется.
         */
         try(DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream())) {
-            /*
-            1. Получаем запрос и обрабатываем его (handle request).
-               Выводим на экран запрос от HttpClientTester клиента.
-            */
+            
+            /* 1. Получаем запрос и обрабатываем его (handle request). Выводим на экран запрос от HttpClientTester клиента. */
             System.out.println("Request: \n" + new String(dataInputStream.readNBytes(456)));
+            
             /* Имитируем замедление обработки запроса от клиента */
             try {
                 Thread.sleep(10000);
@@ -93,12 +78,14 @@ public class PoolThreadHttpServer {
                Это будет тело - body нашего ответа.
             */
             byte[] responseBody = Files.readAllBytes(Path.of("resources","htmlExample.html"));
+            
             /* Формируем header-ы ответа */
             byte[] responseHeaders = """
                     HTTP/1.1 200 OK
                     content-type: text/html
                     content-length: %s
                     """.formatted(responseBody.length).getBytes();
+            
             /* Сервер отправляет ответ клиенту */
             dataOutputStream.write(responseHeaders);
             dataOutputStream.write(System.lineSeparator().getBytes());
@@ -106,11 +93,8 @@ public class PoolThreadHttpServer {
 
         } catch (IOException ioException){
             /*
-            В данном случае мы не пробрасываем исключения выше, а
-            обрабатываем полученные ошибки, чтобы сервер не
-            остановился. Например, мы логгируем ошибки, а сервер
-            продолжает обрабатывать другие запросы, от других
-            клиентов.
+            В данном случае мы не пробрасываем исключения выше, а обрабатываем полученные ошибки, чтобы сервер не остановился. 
+            Например, мы логгируем ошибки, а сервер продолжает обрабатывать другие запросы, от других клиентов.
             */
             ioException.printStackTrace();
         } finally {
