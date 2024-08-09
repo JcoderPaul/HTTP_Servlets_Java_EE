74f496ad456cc5e45ce7dc465caed63f914b9c91
Paul Yermolaeff <100008373+JcoderPaul@users.noreply.github.com>
2024-08-09 17:46:09 +0500

    Update SimpleURLClient.java

--- SocketClasses/src/URL_Client/SimpleURLClient.java
+++ SocketClasses/src/URL_Client/SimpleURLClient.java
@@ -7,21 +7,17 @@ import java.util.Map;
 
 public class SimpleURLClient {
     public static void main(String[] args) throws IOException {
+        
         /*
-        Создаем объект класса URL, который работает по протоколу
-        HTTP. По умолчанию, при открытии соединения, этот объект
-        запрашивает данные с указанного URL адреса. Т.е. формирует
-        запрос *.get и получает соответствующий отклик от сервера,
-        который мы можем извлечь и использовать.
+        Создаем объект класса URL, который работает по протоколу HTTP. По умолчанию, при открытии соединения, этот объект запрашивает данные с 
+        указанного URL адреса. Т.е. формирует запрос *.get и получает соответствующий отклик от сервера, который мы можем извлечь и использовать.
 
-        Естественно он может работать с другими методами HTTP: POST,
-        PUT и т.д.
+        Естественно он может работать с другими методами HTTP: POST, PUT и т.д.
         */
+        
         URL myFiestUrlObject = new URL("https://ya.ru");
-        /* Открываем соединение */
-        URLConnection myFirstUrlConnection = myFiestUrlObject.openConnection();
-        /* Получаем карту полей и их значений из ответа сервера */
-        Map<String, List<String>> urlFields = myFirstUrlConnection.getHeaderFields();
+        URLConnection myFirstUrlConnection = myFiestUrlObject.openConnection();    // Открываем соединение
+        Map<String, List<String>> urlFields = myFirstUrlConnection.getHeaderFields();    // Получаем карту полей и их значений из ответа сервера
 
         for (Map.Entry prn: urlFields.entrySet()) {
             System.out.println(prn.getKey() + " : " + prn.getValue());
